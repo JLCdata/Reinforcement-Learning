db35cbb74e3dd3539f5e051e5152304b0d35778a
JLCdata <jose.luis.cadiz@outlook.com>
2023-05-09 17:41:43 -0400

    Avances T3

--- T3/policy_gradients/code/policy_gradients.py
+++ T3/policy_gradients/code/policy_gradients.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn as nn
 
 import numpy as np
+from torch.optim import AdamW
 
 class Policy(nn.Module):
     
@@ -66,52 +67,121 @@ class PolicyGradients:
 
     def _select_action_discrete(self, observation):
         # sample from categorical distribution
-        pass
+        RN_policy=self._policy 
+        logits=RN_policy(observation)
+
+        # Probabilidad de cada acción
+        probs = torch.softmax(logits, dim=-1)
+
+        # Distribución de probabilidad categorica
+        dist = torch.distributions.Categorical(probs)
+
+        # Sample de acción
+        action = dist.sample()#.item()
+     
+        return action
 
 
     def _select_action_continuous(self, observation):
         # sample from normal distribution
         # use the log std trainable parameter
-        pass
+
+        # RN
+        RN_policy=self._policy
+
+        # Parametro log std de la RN
+        log_std=RN_policy.log_std
+        std = torch.exp(log_std)
+
+        # Politica dada la observación (Representa el promedio de la distribución normal que muestrea acciones)
+        policy=RN_policy(observation)
+        
+        # Distribución normal de parametros mean y std, esta se utiliza para muestrear acciones de modo de tal de explorar el espacio de acciones
+        dist = torch.distributions.Normal(policy, std)
+
+        # sample de acción
+        action = dist.sample()
+        
+        # Asegurarse de que las acciones están dentro del rango [-1, 1]
+        #action = torch.tanh(action)
+        
+        return action
             
 
     def update(self, observation_batch, action_batch, advantage_batch):
         # update the policy here
+
         # you should use self._compute_loss 
-        pass
+        loss=-self._compute_loss(observation_batch, action_batch, advantage_batch)
+        
+        # Backpropagation
+        self._policy.zero_grad()
+        loss.backward()
+        self._optimizer.step()
     
 
     def _compute_loss_discrete(self, observation_batch, action_batch, advantage_batch):
         # use negative logprobs * advantages
-        pass
+        logits=self._policy(observation_batch)
+
+        # Distribución de probabilidad categorica
+        dist = torch.distributions.Categorical(logits=logits)
+        log_probs=dist.log_prob(torch.tensor(action_batch)).squeeze(0)
+        retorno_descontado=torch.tensor(advantage_batch)
+        loss=torch.mean(log_probs*retorno_descontado)#.item()
+        return loss
+        
 
 
     def _compute_loss_continuous(self, observation_batch, action_batch, advantage_batch):
         # use negative logprobs * advantages
-        pass
+        means=self._policy(observation_batch).squeeze(0).squeeze(1)
 
-    
+        log_std=self._policy.log_std
+        std = torch.exp(log_std)
+
+        # Distribución normal de parametros mean y std, esta se utiliza para muestrear acciones de modo de tal de explorar el espacio de acciones
+        dist = torch.distributions.Normal(means, std)
+        log_probs=dist.log_prob(torch.tensor((action_batch))).squeeze(0)
+        retorno_descontado=torch.tensor(advantage_batch)
+        loss=torch.mean(log_probs*retorno_descontado)#.item()
+        return loss
+        
     def estimate_returns(self, rollouts_rew):
         estimated_returns = []
         for rollout_rew in rollouts_rew:
-                
+
+            # Largo del episodio (largo del reward)
+            n_steps = len(rollout_rew[2])
+            estimated_return = np.zeros(n_steps)
+
             if self._use_reward_to_go:
                 # only for part 2
-                estimated_return = None
+                vec_gammas=np.array([self._gamma**j for j in range(n_steps)])
+
+                for t in range(n_steps):
+
+                    sum_descount=np.sum(vec_gammas[t:]*rollout_rew[2][t:])
+                    estimated_return[t] = sum_descount
             else:
-                estimated_return = None
+
+                estimated_return = np.zeros(n_steps)
+
+                vec_gammas=np.array([self._gamma**j for j in range(n_steps)])
+
+                sum_descount=np.sum(vec_gammas*rollout_rew[2])
+
+                for t in range(n_steps):
+                    
+                    estimated_return[t] = sum_descount
+                 
             
             estimated_returns = np.concatenate([estimated_returns, estimated_return])
 
         if self._use_baseline:
             # only for part 2
-            average_return_baseline = None
-            # Use the baseline:
-            #estimated_returns -= average_return_baseline
+            average_return_baseline = np.mean(estimated_returns)
+            estimated_returns -= average_return_baseline
 
         return np.array(estimated_returns, dtype=np.float32)
 
-
-    # It may be useful to discount the rewards using an auxiliary function [optional]
-    def _discount_rewards(self, rewards):
-        pass
